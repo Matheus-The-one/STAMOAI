edf506e9687a193d0f7443509a93d968926d7bfc
matheustheone <matepapava123@gmail.com>
2024-08-14 00:36:47 +0400

    .

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -11,7 +11,7 @@ const Benefits = () => {
       <div className="container relative z-2">
         <Heading
           className="md:max-w-md lg:max-w-2xl"
-          title="Turn Your Words into Dynamic Worlds!"
+          title=""
         />
 
         <div className="flex flex-wrap gap-10 mb-10">
