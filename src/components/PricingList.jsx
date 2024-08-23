55c68983dd796e86939567a5ff89ad74c54ffa24
matheustheone <matepapava123@gmail.com>
2024-08-23 11:29:56 +0400

    adding mail

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -29,7 +29,7 @@ const PricingList = () => {
 
           <Button
             className="w-full mb-6"
-            href={item.price ? "/pricing" : ""}
+            href={item.price ? "/pricing" : "mailto:stamoi.ai.info@gmail.com"}
             white={!!item.price}
           >
             {item.price ? "Get started" : "Contact us"}
